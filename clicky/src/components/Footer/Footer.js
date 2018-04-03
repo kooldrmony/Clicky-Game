e58c93da4b289426910956c144e75cd60319ef36
kooldrmony <derrickreyes.23@gmail.com>
2018-04-03 06:56:04 -0700

    added some updates to css

--- clicky/src/components/Footer/Footer.js
+++ clicky/src/components/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => (
-  <footer class="footer">
-  <p>Footer</p>
-</footer>
+  <header className = "footer">
+    <p>Copyright 2018</p>
+  </header>
 );
 
 export default Footer;
